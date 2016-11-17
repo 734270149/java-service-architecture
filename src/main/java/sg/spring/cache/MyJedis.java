9439094b771b8896539877ecfdd4dd7d8d4a295f
shiguang15 <shiguang3@jd.com>
2016-11-17 14:48:35 +0800

    调整redis配置

--- src/main/java/sg/spring/cache/MyJedis.java
+++ src/main/java/sg/spring/cache/MyJedis.java
@@ -1,17 +1,22 @@
 package sg.spring.cache;
 
+import org.apache.commons.lang3.StringUtils;
+
 import redis.clients.jedis.Jedis;
 
 /**
  * Created by SG on 2016/11/16.
  */
 public class MyJedis extends Jedis {
-    public MyJedis(String host, int port) {
-        super(host, port);
-    }
 
-    public MyJedis(String host, int port, String password) {
-        super(host, port);
-        auth(password);
+  public MyJedis(String host, int port) {
+    super(host, port);
+  }
+
+  public MyJedis(String host, int port, String password) {
+    super(host, port);
+    if (StringUtils.isNotBlank(password)) {
+      auth(password);
     }
+  }
 }
